8dcd9c65a25f2a5b45b1506734c59f2a59c3b479
Paryank Namdeo <paryank.namdeo@sap.com>
2024-11-25 19:03:54 +0530

    import bug

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,6 @@ import { RequireAuth } from './helpers/RequireAuth';
 import { RequireNoAuth } from './helpers/RequireNoAuth';
 import RootRedirect from './components/auth/rootRedirect';
 import AdminDashboard from './dashboards/AdminDashboard';
-import UsersDashboard from './dashboards/UsersDashboard';
 function App() {
   return (
     <div className='App'>
@@ -18,9 +17,6 @@ function App() {
           <Route path='/signin' element={<RequireNoAuth><Signin/></RequireNoAuth>}/>
           <Route path='/signup' element={<RequireNoAuth><Signup/></RequireNoAuth>}/>
           <Route path='/admin-dashboard' element={<RequireAuth><AdminDashboard/></RequireAuth>}/>
-          <Route path='/user-dashboard' element={<RequireAuth><UsersDashboard/></RequireAuth>}/>
-
-          
         </Routes>
       </AuthProvider>
     </div>
